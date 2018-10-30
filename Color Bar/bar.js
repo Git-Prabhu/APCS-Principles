b3e68a6449871ab761245104e03864516ae55f2f
Dean Davis <792928@seq.org>
2018-10-30 08:49:36 -0700

    10/30
    color bars edit

--- Color Bar/bar.js
+++ Color Bar/bar.js
@@ -1,1 +1,20 @@
-start code time
+// making the bars
+var b = 50 // number of bars
+var w = windowWidth/b //making the width of the bars
+function bar (loc, width, col, height){
+  this.loc = loc;
+  this.width = width;
+  this.col = col;
+  this.height = height;
+
+  this.run = function(){
+    this.render();
+
+    //render draws the bars at different locs
+    this.render = function(){
+      //draws bars
+      fill(random(255,255,255))
+      rect(this.loc.x, this.loc.y, this.w, this.h)
+    }
+  }
+}
