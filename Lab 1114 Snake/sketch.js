c77408f429d4da51bceeac0866bc1bca87d6fb93
Russell Davis <rdavis@WS-ACADBUS.EDU>
2018-11-26 08:50:13 -0800

    11/16/18

--- Lab 1114 Snake/sketch.js
+++ Lab 1114 Snake/sketch.js
@@ -8,3 +8,13 @@ var w = 20;
 //construct a grid in the canvas
 //make a function that
 //
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20,20,20);
+
+  function draw() {
+    background(20,20,20);
+    snake.run;
+
+    function keypressed();
